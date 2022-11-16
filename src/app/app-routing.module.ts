d74dd4286899223a25961bd1b0b5ed6819c2044d
Matthieu Herrb <anisdarghouthii@gmail.com>
2022-11-16 17:06:01 +0100

    paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MatchesComponent } from './components/matches/matches.component';
+import { PlayersComponent } from './components/players/players.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:SignupComponent},
+  {path:'matches', component:MatchesComponent},
+  {path:'allPlayers', component:PlayersComponent}
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,18 @@ import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { CupEventComponent } from './components/cup-event/cup-event.component';
+import { ScoreComponent } from './components/score/score.component';
+import { NewsComponent } from './components/news/news.component';
+import { RankingComponent } from './components/ranking/ranking.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { MatchesComponent } from './components/matches/matches.component';
+import { MatchComponent } from './components/match/match.component';
+import { PlayersComponent } from './components/players/players.component';
+import { PlayerInfoComponent } from './components/player-info/player-info.component';
+import { AddMatchComponent } from './components/add-match/add-match.component';
+import { AddTeamComponent } from './components/add-team/add-team.component';
+import { AddPlayerComponent } from './components/add-player/add-player.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +28,19 @@ import { SignupComponent } from './components/signup/signup.component';
     FooterComponent,
     LoginComponent,
     HomeComponent,
-    SignupComponent
+    SignupComponent,
+    CupEventComponent,
+    ScoreComponent,
+    NewsComponent,
+    RankingComponent,
+    BlogComponent,
+    MatchesComponent,
+    MatchComponent,
+    PlayersComponent,
+    PlayerInfoComponent,
+    AddMatchComponent,
+    AddTeamComponent,
+    AddPlayerComponent
   ],
   imports: [
     BrowserModule,
